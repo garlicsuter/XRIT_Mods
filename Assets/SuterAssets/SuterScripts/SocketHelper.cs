c2b8de17ba6608cd39ff6b8ed4f35531c6087bff
GarlicSuter <markasuter@gmail.com>
2024-02-13 08:32:13 -0500

    setup for controller data monitoring

--- Assets/SuterAssets/SuterScripts/SocketHelper.cs
+++ Assets/SuterAssets/SuterScripts/SocketHelper.cs
@@ -1,7 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
+/// <summary>
+///     Instructions:    
+///     1) Attach this SocketHelper.cs to the Interactable GameObject (the thing you pick up)
+///     2) Assign variables in Inspector
+///     3) On the socket, set tag to "Socket".
+/// </summary>
+
 
 public class SocketHelper : MonoBehaviour
 {
@@ -9,14 +17,37 @@ public class SocketHelper : MonoBehaviour
     public GameObject Cheese;
     public GameObject fakeCheese;
     public GameObject theSocket;
-
-    [SerializeField]
+    public bool inRange = false;
     public float snapDuration = 2.0f;
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Socket"))
+        {
+            Debug.Log("Socket Hit");
+            inRange = true;
+            ///other thing is a socket
+            theSocket = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Socket"))
+        {
+            inRange = false;
+            Debug.Log("Socket Exited");
+            ///other thing is a socket
+            theSocket = null;
+        }
+    }
+
+    //EasePlease() should ONLY run when BOTH the grip is released AND this object is triggerenter in the tagged "socket" gameobject. 
     public void EasePlease()
     {
         MatchPosition();
-        Cheese.SetActive(false);
+        //Cheese.SetActive(false);
         fakeCheese.SetActive(true);
         
         //Setting isEasing to true only for testing the function. It will be set on/off when starting the snapDuration
@@ -41,13 +72,6 @@ public class SocketHelper : MonoBehaviour
         fakeCheese.transform.position = Cheese.transform.position;
         fakeCheese.transform.rotation = Cheese.transform.rotation;
         fakeCheese.transform.localScale = Cheese.transform.localScale;
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Socket"))
-        {
-            Debug.Log("Socket Hit");
-        }
+        Debug.Log("Position Matched!");
     }
 }
